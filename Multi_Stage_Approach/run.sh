33efc4eb297df0c35ada850031679019a99f8efe
3030curry30 <32303007+3030curry30@users.noreply.github.com>
2022-03-03 17:08:11 +0800

    Update run.sh

--- Multi_Stage_Approach/run.sh
+++ Multi_Stage_Approach/run.sh
@@ -1,3 +1,9 @@
+# first stage.
 CUDA_VISIBLE_DEVICES=5,6 python main.py --file_type="car" --model_mode="bert" --program_mode="run" --stage_model="first" --epoch=25 --model_type="multitask" --embed_dropout=0.1 --premodel_path="/home/pretrain_model/"
 CUDA_VISIBLE_DEVICES=5,6 python main.py --file_type="ele" --model_mode="bert" --program_mode="run" --stage_model="first" --epoch=25 --model_type="multitask" --embed_dropout=0.1 --premodel_path="/home/pretrain_model/"
 CUDA_VISIBLE_DEVICES=5,6 python main.py --file_type="kesserl14" --model_mode="bert" --program_mode="run" --stage_model="first" --epoch=25 --model_type="multitask" --embed_dropout=0.1 --premodel_path="/home/pretrain_model/"
+
+# second and thrid stage.
+CUDA_VISIBLE_DEVICES=5,6 python main.py --file_type="car" --model_mode="bert" --program_mode="run" --stage_model="second" --epoch=25 --model_type="multitask" --embed_dropout=0.1 --premodel_path="/home/pretrain_model/" --factor=0.3
+CUDA_VISIBLE_DEVICES=5,6 python main.py --file_type="ele" --model_mode="bert" --program_mode="run" --stage_model="second" --epoch=25 --model_type="multitask" --embed_dropout=0.1 --premodel_path="/home/pretrain_model/" --factor=0.3
+CUDA_VISIBLE_DEVICES=5,6 python main.py --file_type="kesserl14" --model_mode="bert" --program_mode="run" --stage_model="second" --epoch=25 --model_type="multitask" --embed_dropout=0.1 --premodel_path="/home/pretrain_model/" --factor=0.3
